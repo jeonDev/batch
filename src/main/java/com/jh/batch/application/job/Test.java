b5490bcbbccf483f73c5576d3758868db61ec52c
jeonDev <jjhwhdgus0727@gmail.com>
2024-12-26 13:59:50 +0900

    feat : Fixed Length

--- src/main/java/com/jh/batch/application/job/Test.java
+++ src/main/java/com/jh/batch/application/job/Test.java
@@ -2,9 +2,11 @@ package com.jh.batch.application.job;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Test {
 
     private String a;
--- src/main/java/com/jh/batch/application/job/TestJobConfig.java
+++ src/main/java/com/jh/batch/application/job/TestJobConfig.java
@@ -12,6 +12,7 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.*;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.Range;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
@@ -42,7 +43,7 @@ public class TestJobConfig {
                 .processor(new ItemProcessor<Object, Object>() {
                     @Override
                     public Object process(Object item) throws Exception {
-                        log.info("222");
+                        log.info(item.toString());
                         return null;
                     }
                 })
@@ -61,12 +62,27 @@ public class TestJobConfig {
 
         return new FlatFileItemReaderBuilder<Test>()
                 .name("testReader")
-                .resource(new FileSystemResource(filePath + "TEST1"))
-                .encoding("UTF-8")
-                .delimited().delimiter(".")
-                .names("a", "b", "c", "d")
-                .targetType(Test.class)
-                .linesToSkip(0)
+                .resource(new FileSystemResource(filePath + "test")) // 파일 경로
+                .encoding("UTF-8") // 인코딩 설정
+                .fixedLength() // FixedLength 방식 사용
+                .columns(new Range(1, 4), new Range(5, 8), new Range(9, 12), new Range(13, 16)) // 고정 길이 지정 (시작 인덱스, 종료 인덱스)
+                .names("a", "b", "c", "d") // 필드 이름
+                .targetType(Test.class) // 매핑할 클래스
                 .build();
     }
+
+//    @Bean
+//    @StepScope
+//    public FlatFileItemReader<Test> testReader() {
+//
+//        return new FlatFileItemReaderBuilder<Test>()
+//                .name("testReader")
+//                .resource(new FileSystemResource(filePath + "TEST1"))
+//                .encoding("UTF-8")
+//                .delimited().delimiter(".")
+//                .names("a", "b", "c", "d")
+//                .targetType(Test.class)
+//                .linesToSkip(0)
+//                .build();
+//    }
 }
